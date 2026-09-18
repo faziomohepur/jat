8febed8de61a8238db4e13920189dd784b986187
faziomohepur <faziomohepur@gmail.com>
2026-09-18 16:49:44 +0700

    a

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ export default {
 
       // For other images, proxy to Netlify
       const netlifyUrl = new URL(url.toString());
-      netlifyUrl.hostname = 'xedienat.netlify.app';
+      netlifyUrl.hostname = 'xedienat.tmc.io.vn';
       return fetch(netlifyUrl);
     }
 
@@ -35,7 +35,7 @@ export default {
 
     // For all other requests, proxy to Netlify
     const netlifyUrl = new URL(url.toString());
-    netlifyUrl.hostname = 'xedienat.netlify.app';
+    netlifyUrl.hostname = 'xedienat.tmc.io.vn';
 
     const proxyRequest = new Request(netlifyUrl, {
       method: request.method,
